9fd206430487c8be80b1bf3d88de6a8a5af29460
Tanachai Bunlutangtum <tanachai.bun@gmail.com>
2024-05-07 20:19:39 +0700

    fix google font loading

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,11 +6,6 @@ import { Inter, Mali, Noto_Sans_Thai, Playpen_Sans } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 const notoSansThai = Noto_Sans_Thai({ subsets: ["thai"] });
-const playpenSans = Playpen_Sans({ subsets: ["latin"] });
-const mali = Mali({
-  subsets: ["thai"],
-  weight: ["200", "300", "400", "500", "600", "700"],
-});
 
 export const metadata: Metadata = { title: "TBUN Tools" };
 
@@ -22,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full">
       <body
-        className={`${mali.className} font-default bg-bg text-text_grey text-xs h-full`}
+        className={`${inter.className} ${notoSansThai.className} font-default bg-bg text-text_grey text-xs h-full`}
       >
         {children}
       </body>
